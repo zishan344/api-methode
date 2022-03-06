a6080aa838f8edd5675d4c9e3c2594049f622411
zishan344 <zishanahmed344@gmail.com>
2022-03-06 09:35:04 +0600

    done the practice

--- shoping-2.js
+++ shoping-2.js
@@ -1,1 +1,54 @@
 // console.log("hello world");
+const inputId = document.getElementById("input-field");
+
+const ul = document.getElementById("added-cart");
+const displayLocalStorageCart = () => {
+  const cart = getItem();
+  for (const carts in cart) {
+    displayProduct(carts);
+  }
+};
+
+const addedCart = () => {
+  const inputIdValue = inputId.value;
+  if (!inputIdValue) {
+    return;
+  }
+  displayProduct(inputIdValue);
+
+  // added local storage
+  addtoCart(inputIdValue);
+  inputId.value = "";
+};
+const displayProduct = (name) => {
+  const li = document.createElement("li");
+  li.innerText = name;
+  ul.appendChild(li);
+};
+
+const getItem = () => {
+  const cart = localStorage.getItem("cart");
+  let cartObj;
+  if (cart) {
+    cartObj = JSON.parse(cart);
+  } else {
+    cartObj = {};
+  }
+  return cartObj;
+};
+const addtoCart = (name) => {
+  const cart = getItem();
+  if (cart[name]) {
+    cart[name] += 1;
+  } else {
+    cart[name] = 1;
+  }
+
+  const stringifyeed = JSON.stringify(cart);
+  localStorage.setItem("cart", stringifyeed);
+};
+displayLocalStorageCart();
+const placeOrder = () => {
+  ul.textContent = "";
+  localStorage.removeItem("cart");
+};
